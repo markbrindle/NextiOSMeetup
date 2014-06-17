477112cf76e1269a64e2bd854c0f240085a0b573
Mark Brindle <mark.brindle@arkemm.com>
2014-06-17 19:43:48 +0100

    Initial Commit

---
+++ NextiOSMeetup/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NextiOSMeetup
+//
+//  Created by Mark Brindle on 17/06/2014.
+//  Copyright (c) 2014 Arkemm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NextiOSMeetup/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NextiOSMeetup
+//
+//  Created by Mark Brindle on 17/06/2014.
+//  Copyright (c) 2014 Arkemm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
