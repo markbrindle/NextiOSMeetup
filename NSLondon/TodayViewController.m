6cfd79dee416d5899ecc58bc05d502ce9077f86f
Karol Kozimor <karol.kozimor@iosyn.com>
2014-06-22 23:23:48 +0100

    Moved the NSURLSessionTask to viewDidLoad so it has a chance of completing.

--- NSLondon/TodayViewController.m
+++ NSLondon/TodayViewController.m
@@ -11,6 +11,7 @@
 
 @interface TodayViewController () <NCWidgetProviding>
 @property (weak, nonatomic) IBOutlet UILabel *label;
+@property (strong, nonatomic) NSString *apiKey;
 @end
 
 @implementation TodayViewController
@@ -25,7 +26,20 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.label.text = @"NSLondon is happening right now!";
+
+    self.label.text = @"Loading …";
+    NSURL *URL = [self eventURL];
+    NSURLSessionDataTask *task = [[NSURLSession sharedSession] dataTaskWithURL:URL completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        if (error) {
+            self.label.text = error.localizedDescription;
+        }
+        else {
+            NSDictionary *JSON = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+            NSDictionary *nextEvent = JSON[@"results"][0];
+            [self updateLabelWithEventInfo:nextEvent];
+        }
+    }];
+    [task resume];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -33,14 +47,61 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (void)widgetPerformUpdateWithCompletionHandler:(void (^)(NCUpdateResult))completionHandler {
-    // Perform any setup necessary in order to update the view.
-    
-    // If an error is encoutered, use NCUpdateResultFailed
-    // If there's no update required, use NCUpdateResultNoData
-    // If there's an update, use NCUpdateResultNewData
+- (NSURL *)eventURL
+{
+    if ([self.apiKey length] > 0) {
+        return [NSURL URLWithString:[NSString stringWithFormat:@"https://api.meetup.com/2/events?&sign=true&group_urlname=NSLondon&page=20&key=%@", self.apiKey]];
+    }
+    else {
+        self.label.text = @"Provide a valid API key!";
+    }
+    return nil;
+}
 
-    completionHandler(NCUpdateResultNewData);
+- (void)updateLabelWithEventInfo:(NSDictionary *)eventDict
+{
+    NSString *labelText;
+    NSString *status = eventDict[@"status"];
+    NSTimeInterval eventTime = [eventDict[@"time"] doubleValue] / 1000;
+    BOOL upcoming = [status isEqualToString:@"upcoming"] && eventTime > [[NSDate date] timeIntervalSince1970];
+    if (upcoming) {
+        NSDate *eventDate = [NSDate dateWithTimeIntervalSince1970:eventTime];
+        NSString *dateString = [[self dateFormatter] stringFromDate:eventDate];
+        labelText = [NSString stringWithFormat:@"Next NSLondon is on %@", dateString];
+    }
+    else {
+        labelText = @"Ask @Daniel1of1 about the next NSLondon!";
+    }
+    self.label.text = labelText;
+}
+
+- (NSDateFormatter *)dateFormatter
+{
+    static NSDateFormatter *_dateFormatter;
+    if (!_dateFormatter) {
+        _dateFormatter = [[NSDateFormatter alloc] init];
+        [_dateFormatter setDateStyle:NSDateFormatterShortStyle];
+        [_dateFormatter setTimeStyle:NSDateFormatterShortStyle];
+    }
+    return _dateFormatter;
+}
+
+- (void)widgetPerformUpdateWithCompletionHandler:(void (^)(NCUpdateResult))completionHandler {
+    /* The widget gets killed before the call completes, needs a background session shared with the container app */
+    NSURL *URL = [self eventURL];
+    NSURLSessionDataTask *task = [[NSURLSession sharedSession] dataTaskWithURL:URL completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        if (error) {
+            self.label.text = @"Failed :(";
+            completionHandler(NCUpdateResultFailed);
+        }
+        else {
+            NSDictionary *JSON = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+            NSDictionary *nextEvent = JSON[@"results"][0];
+            [self updateLabelWithEventInfo:nextEvent];
+            completionHandler(NCUpdateResultNewData);
+        }
+    }];
+    [task resume];
 }
 
 @end
